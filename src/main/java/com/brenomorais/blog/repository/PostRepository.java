46b045b31320ee6eaaf8b2de532273ec5f9b356b
breno morais <breno.lopes.morais@hotmail>
2018-08-17 18:24:35 -0300

    implementando classe repository

---
+++ src/main/java/com/brenomorais/blog/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.brenomorais.blog.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.brenomorais.blog.model.Post;
+
+@Repository
+public interface PostRepository  extends JpaRepository<Post, Long> {
+
+}
